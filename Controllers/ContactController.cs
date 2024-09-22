defe6e86a1aecc9a4db468e97db1eaf1f71bf0c8
taluong13 <tailuongbr13@gmail.com>
2024-09-22 14:13:36 +0700

    Update /9/2022

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TL13Shop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
